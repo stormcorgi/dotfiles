6f8592380225a1fa970c1635eb2ed5bc79901a23
noizms <noizms@gmail.com>
2018-03-28 10:26:46 +0900

    oh my zsh rewrites .zshrc linkage. change order to resolve that problem.

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
+sudo sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
-sudo sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
