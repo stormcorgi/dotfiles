bc81e75eb3d7e7c46e6af4a43ce16a94cbb53b02
noizms <noizms@gmail.com>
2020-10-23 12:08:32 +0900

    sapalate vim-setup

--- setup.sh
+++ setup.sh
@@ -27,29 +27,7 @@ done
 echo "zsh initialize section done!"
 
 ## vim section
-# vim dotfiles
-ln -sf $script_dir/vim/vimrc ~/.vimrc
-# create vim undo / swp / backup target dirctory
-if [ ! -e ~/.vim/undo ];then
-    echo "created undo/swp/backup target dir: ~/.vim/undo"
-    mkdir -p ~/.vim/undo
-fi
-# Vundle install
-# vim-lsp require ~/.cache/tmp
-mkdir -p ~/.cache/tmp
-if [ ! -e ~/.vim/bundle/Vundle.vim ];then
-    echo "~/.vim/bundle/Vundle.vim not fonud. installing..."
-    mkdir -p ~/.vim/bundle
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-    vim +PluginInstall +qall
-fi
-
-# Vim colorsheme installation
-mkdir -p ~/.vim/colors
-git clone https://github.com/tomasr/molokai /tmp/molokai
-mv /tmp/molokai/colors/molokai.vim ~/.vim/colors/
-
-echo "vim initialize section done!"
+/bin/sh ./vim-setup.sh
 
 # english -> japanese dictionary data set.
 if [ ! -e ~/.dict/ ];then
