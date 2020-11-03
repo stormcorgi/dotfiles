6d8de20164f77857df9f6cde5c90214046ade5de
StormCorgi <70886974+StormCorgi@users.noreply.github.com>
2020-11-03 11:25:51 +0900

    split vim intiialization

--- setup.sh
+++ setup.sh
@@ -42,32 +42,7 @@ done
 echo "zsh initialize section done!"
 
 ## vim section
-# vim dotfiles
-ln -sf $script_dir/vim/vimrc ~/.vimrc
-# create vim undo / swp / backup target dirctory
-if [ ! -e ~/.vim/undo ];then
-    echo "created undo/swp/backup target dir: ~/.vim/undo"
-    mkdir -p ~/.vim/undo
-fi
-# Vundle install
-# vim-lsp require ~/.cache/tmp
-mkdir -p ~/.cache/tmp
-if [ ! -e ~/.vim/bundle/Vundle.vim ];then
-    echo "~/.vim/bundle/Vundle.vim not fonud. installing..."
-    mkdir -p ~/.vim/bundle
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-    vim +PluginInstall +qall
-fi
-
-# Vim colorsheme installation
-if [ ! -e ~/.vim/colors ];then
-    mkdir -p ~/.vim/colors
-    git clone https://github.com/tomasr/molokai /tmp/molokai
-    mv /tmp/molokai/colors/molokai.vim ~/.vim/colors/
-    rm -rf /tmp/molokai
-fi
-
-echo "vim initialize section done!"
+/bin/sh ./vim_setup.sh
 
 # create local zsh file.
 touch ~/.zshrc.local
---
+++ vim_setup.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+## vim section
+# vim dotfiles
+ln -sf $script_dir/vim/vimrc ~/.vimrc
+# create vim undo / swp / backup target dirctory
+if [ ! -e ~/.vim/undo ];then
+    echo "created undo/swp/backup target dir: ~/.vim/undo"
+    mkdir -p ~/.vim/undo
+fi
+echo "tmpdir(~/.vim/undo) generated."
+
+# Vundle install
+# vim-lsp require ~/.cache/tmp
+mkdir -p ~/.cache/tmp
+if [ ! -e ~/.vim/bundle/Vundle.vim ];then
+    echo "~/.vim/bundle/Vundle.vim not fonud. installing..."
+    mkdir -p ~/.vim/bundle
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    vim +PluginInstall +qall
+fi
+echo "Vundle install done."
+
+# Vim colorsheme installation
+if [ ! -e ~/.vim/colors ];then
+    mkdir -p ~/.vim/colors
+    git clone https://github.com/tomasr/molokai /tmp/molokai
+    mv /tmp/molokai/colors/molokai.vim ~/.vim/colors/
+    rm -rf /tmp/molokai
+fi
+
+echo "vim initialize section done!"
+
+cd $CURDIR
+
+
