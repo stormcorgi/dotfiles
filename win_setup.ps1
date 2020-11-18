1fd3efc452008c8b07ea4ae1ee23fc67bf82df00
StormCorgi <70886974+StormCorgi@users.noreply.github.com>
2020-11-18 15:41:01 +0900

    powershell auto-format

--- win_setup.ps1
+++ win_setup.ps1
@@ -1,11 +1,11 @@
 # scoop install
-if (!(get-command "scoop.ps1")) {
-    echo "scoop(windows package manager) isn't installed. so install it."
-    Set-ExecutionPolicy RemoteSigned -scope CurrentUser
+if (!(Get-Command "scoop.ps1")) {
+    Write-Output "scoop(windows package manager) isn't installed. so install it."
+    Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
     Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
-    echo "scoop install : done!"
+    Write-Output "scoop install : done!"
 } else {
-    echo "scoop is already installed."
+    Write-Output "scoop is already installed."
 }
 
 # scoop periferals
@@ -14,19 +14,19 @@ scoop install 7zip busybox conemu dark dig innounp peco putty pwsh
 
 
 # peco install
-if (!(get-command "peco.exe")) {
-    echo "peco isn't installed. so install it."
+if (!(Get-Command "peco.exe")) {
+    Write-Output "peco isn't installed. so install it."
     scoop install peco
-    echo "peco install : done!"
+    Write-Output "peco install : done!"
 } else {
-    echo "peco is already installed."
+    Write-Output "peco is already installed."
 }
 
 # pwsh(powershell core) installed?
-if (!(get-command "pwsh.exe")) {
+if (!(Get-Command "pwsh.exe")) {
     scoop install pwsh
-    echo "you must use pwsh. call this ps1 file with pwsh."
-    echo "> pwsh ./setup.ps1"
+    Write-Output "you must use pwsh. call this ps1 file with pwsh."
+    Write-Output "> pwsh ./setup.ps1"
     exit
 }
 
@@ -37,42 +37,42 @@ Import-Module oh-my-posh
 Set-Theme Powerline
 "@
 
-if (! ((get-module -ListAvailable -name posh-git) -and (get-module -ListAvailable -Name oh-my-posh)) ) {
+if (! ((Get-Module -ListAvailable -Name posh-git) -and (Get-Module -ListAvailable -Name oh-my-posh)) ) {
     Install-Module posh-git -Scope CurrentUser
     Install-Module oh-my-posh -Scope CurrentUser
     if ($PSVersionTable.PSVersion.Major -gt 6) {
         Install-Module -Name PSReadLine -AllowPrerelease -Scope CurrentUser -Force -SkipPublisherCheck
     }
 
-    echo "add those lines in $profile."
-    echo $(cat $addlines)
+    Write-Output "add those lines in $profile."
+    Write-Output $(Get-Content $addlines)
 
     Import-Module posh-git
     Import-Module oh-my-posh
     Set-Theme Powerline
 } else {
-    echo "posh-git and oh-my-posh is already installed."
+    Write-Output "posh-git and oh-my-posh is already installed."
 }
 
 # vim check (not install)
-if (get-command "vim.exe" -ErrorAction SilentlyContinue) {
+if (Get-Command "vim.exe" -ErrorAction SilentlyContinue) {
     # vim dotfiles cp
-    cp ~/dotfiles/vim/vimrc ~/_vimrc
-    cp ~/dotfiles/vim/gvimrc ~/_gvimrc
+    Copy-Item ~/dotfiles/vim/vimrc ~/_vimrc
+    Copy-Item ~/dotfiles/vim/gvimrc ~/_gvimrc
     # create vim undo / swp / backup target dirctory
-    if (!(test-path ~/.vim/undo)) {
+    if (!(Test-Path ~/.vim/undo)) {
         mkdir ~/.vim/undo
     }
     # Vundle install
-    if (!(test-path ~/.vim/bundle/Vundle.vim)) {
-        echo "~/.vim/bundle/Vundle.vim not fonud. installing..."
+    if (!(Test-Path ~/.vim/bundle/Vundle.vim)) {
+        Write-Output "~/.vim/bundle/Vundle.vim not fonud. installing..."
         mkdir ~/.vim/bundle
-        echo "git clone..."
+        Write-Output "git clone..."
         git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
         vim +PluginInstall +qall
     }
-    echo "vim initialize : done!"
+    Write-Output "vim initialize : done!"
 } else {
-    echo "vim is not installed. so initialization skipped. (require kaoriya-vim x86)"
+    Write-Output "vim is not installed. so initialization skipped. (require kaoriya-vim x86)"
 }
 
