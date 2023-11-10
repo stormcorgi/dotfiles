aaf9db318840e52a8b45a150aeade6224e0233d2
stormcorgi <70886974+stormcorgi@users.noreply.github.com>
2023-11-10 22:42:23 +0900

    Follow setupscript from vim to neovim

--- nvim_setup.sh
+++ nvim_setup.sh
@@ -1,19 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 CURDIR=$(pwd)
 script_dir=$(
-    cd "$(dirname $0)" || exit
-    pwd
+	cd "$(dirname $0)" || exit
+	pwd
 )
 cd "$script_dir" || exit
-
-## vim section
-ln -sf "$script_dir"/vim/vimrc ~/.vimrc
-# create vim undo / swp / backup target dirctory
-if [ ! -e ~/.vim/undo ]; then
-    echo "create tmpdir(~/.vim/undo) for undo/swp/backup"
-    mkdir -p ~/.vim/undo
-    echo "tmpdir generated."
-fi
 
 # required
 mv ~/.config/nvim{,.bak}
