693eb65cbd3da9d7d9d41346ee20d0f1b64db01a
stormcorgi <70886974+stormcorgi@users.noreply.github.com>
2023-11-10 22:40:42 +0900

    Add Wezterm configurations

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,61 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+local act = wezterm.action
+config.leader = { key = "b", mods = "CTRL", timeout_milliseconds = 1000 }
+config.keys = {
+	{
+		key = "%",
+		mods = "LEADER|SHIFT",
+		action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = '"',
+		mods = "LEADER|SHIFT",
+		action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "o",
+		mods = "LEADER",
+		action = act.ActivatePaneDirection("Next"),
+	},
+	{
+		key = "z",
+		mods = "LEADER",
+		action = act.TogglePaneZoomState,
+	},
+}
+
+-- display preference
+config.color_scheme = "Embers (dark)(terminal.sexy)"
+config.font_size = 14
+config.hide_tab_bar_if_only_one_tab = true
+config.window_background_opacity = 0.75
+-- background
+local dimmer = { brightness = 0.025 }
+config.background = {
+	{
+		source = {
+			File = "/Users/n-saito/.config/wezterm/bg_lucy.jpg",
+		},
+		width = "100%",
+		repeat_x = "NoRepeat",
+		vertical_align = "Bottom",
+		repeat_y_size = "100%",
+		hsb = dimmer,
+		attachment = { Parallax = 0.1 },
+	},
+}
+
+-- and finally, return the configuration to wezterm
+return config
---
+++ wezterm_setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mkdir -p ~/.config/wezterm
+ln -sf "${HOME}/dotfiles/wezterm/wezterm.lua" "${HOME}/.config/wezterm/wezterm.lua"
